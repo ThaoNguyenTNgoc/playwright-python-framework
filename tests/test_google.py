8ac35f22aff5be46beb0f78acd5aaf1394630001
ThaoNguyenTNgoc <110299597+ThaoNguyenTNgoc@users.noreply.github.com>
2026-05-19 23:03:11 +0700

    first commit

---
+++ tests/test_google.py
@@ -0,0 +1,20 @@
+from playwright.sync_api import sync_playwright
+
+
+def test_open_google():
+
+    with sync_playwright() as p:
+
+        browser = p.chromium.launch(
+            headless=False
+        )
+
+        page = browser.new_page()
+
+        page.goto("https://google.com")
+
+        print(page.title())
+
+        page.wait_for_timeout(3000)
+
+        browser.close()
